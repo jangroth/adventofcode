4f204ee0597ae0ad159fbde1198bcac1892f498f
Jan Groth <jan.groth@versent.com.au>
2018-12-06 22:06:25 +1100

    Day 3 challenge 1

--- aoc/day03.py
+++ aoc/day03.py
@@ -1,8 +1,36 @@
+import collections
+
+
 class SliceThePatch:
+    MATRIX_SIZE = 100
+
     def __init__(self, path):
         with open(path) as f:
             content = f.readlines()
         self.content = [x.strip() for x in content]
+        self.patches = [self._line_to_patch(x) for x in self.content]
+        self.matrix = self._create_empty_matrix(1000)
+
+    def _create_empty_matrix(self, size):
+        return ['0' * size for _ in range(size)]
+
+    def _place_patches(self):
+        for patch in patches:
+            self._place_patch(patch)
+
+    def _place_patch(self, patch):
+        for x in range(patch.x1, patch.x2):
+            for y in range(patch.y1, patch.x2):
+                self.matrix[x][y] = '1'
+
+    def _line_to_patch(self, line):
+        Patch = collections.namedtuple('Patch', ['x1', 'y1', 'x2', 'y2'])
+        line_elements = line.split()
+        x1 = int(line_elements[2].split(',')[0])
+        y1 = int(line_elements[2].split(',')[1][:-1])
+        x2 = int(line_elements[3].split('x')[0]) + x1
+        y2 = int(line_elements[3].split('x')[1]) + y1
+        return Patch(x1, y1, x2, y2)
 
     def count_overlap(self):
         pass
--- test/test_sliceThePatch.py
+++ test/test_sliceThePatch.py
@@ -1,3 +1,4 @@
+import collections
 from unittest import TestCase
 
 from aoc.day03 import SliceThePatch
@@ -5,7 +6,30 @@ from aoc.day03 import SliceThePatch
 
 class TestSliceThePatch(TestCase):
 
-    def test_count_overlap(self):
+    def test_convert_line_to_patch(self):
+        slice_the_patch = SliceThePatch.__new__(SliceThePatch)
+        Patch = collections.namedtuple('Patch', ['x1', 'y1', 'x2', 'y2'])
+
+        self.assertEqual(Patch(0, 0, 1, 1), slice_the_patch._line_to_patch('#1 @ 0,0: 1x1'))
+        self.assertEqual(Patch(1, 1, 2, 2), slice_the_patch._line_to_patch('#1 @ 1,1: 1x1'))
+        self.assertEqual(Patch(1, 3, 5, 7), slice_the_patch._line_to_patch('#1 @ 1,3: 4x4'))
+
+    def test_create_empty_matrix(self):
+        slice_the_patch = SliceThePatch.__new__(SliceThePatch)
+
+        self.assertEqual(['00', '00'], slice_the_patch._create_empty_matrix(2))
+        self.assertEqual(['000', '000', '000'], slice_the_patch._create_empty_matrix(3))
+
+    def test_place_single_patch(self):
+        slice_the_patch = SliceThePatch.__new__(SliceThePatch)
+        slice_the_patch.matrix = slice_the_patch._create_empty_matrix(4)
+        Patch = collections.namedtuple('Patch', ['x1', 'y1', 'x2', 'y2'])
+
+        slice_the_patch._place_patch(Patch(1, 1, 2, 2))
+
+        self.assertEqual(['0000', '0110', '0110', '0000'], slice_the_patch.matrix)
+
+    def ignore_test_count_overlap(self):
         slice_the_patch = SliceThePatch.__new__(SliceThePatch)
         slice_the_patch.content = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
 
